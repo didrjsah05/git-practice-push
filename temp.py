afbe7d93c9cf59660e78431a2e73e31df89bd748
didrjsah05 <didrjsah05@raonfriends.com>
2025-04-29 14:19:43 +0900

    second commit

--- temp.py
+++ temp.py
@@ -21,7 +21,7 @@ def binary_search(list, target, low=None, high=None):
         return midpoint
 
 if __name__ == "__main__":
-    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     
     a = binary_search(list, 5)
     
